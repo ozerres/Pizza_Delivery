2d7ae1d85874a2112b2e9d7e7900aff395148b11
ozerres <eskimezozer@gmail.com>
2024-04-30 16:04:16 +0300

    Added last things and finishing project
    Map completed.
    Added scripts to when you take boost, your speed going faster and when you bumped, your speed going slower.

--- Assets/Scripts/Delivery.cs
+++ Assets/Scripts/Delivery.cs
@@ -4,21 +4,44 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
-    void OnCollisionEnter2D()
+
+    [SerializeField] Color32 hasPackageColor = new Color32(1, 1, 1, 1);
+    [SerializeField] Color32 noPackageColor = new Color32(1, 1, 1, 1);
+
+
+    [SerializeField] float destroyDelay = 0.5f;
+    bool hasPackage;
+
+    SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();    
+    }
+
+    
+
+    void OnCollisionEnter2D(Collision2D collision)
     {
         Debug.Log("Bumped!");
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.tag == "Package")
+        if (other.tag == "Pizza" && !hasPackage)
         {
-            Debug.Log("Package picked up !");
+            Debug.Log("Pizza picked up !");
+            hasPackage = true;
+            spriteRenderer.color = hasPackageColor;
+            Destroy(other.gameObject, destroyDelay);
         }
 
-        if (other.tag == "Customer")
+        if (other.tag == "Customer" && hasPackage)
         {
-            Debug.Log("Package delivered !");
+            Debug.Log("Pizza delivered !");
+            hasPackage = false;
+            spriteRenderer.color = noPackageColor;
+            Destroy(other.gameObject, destroyDelay);
         }
     }
 }
--- Assets/Scripts/Driver.cs
+++ Assets/Scripts/Driver.cs
@@ -6,13 +6,9 @@ public class Driver : MonoBehaviour
 {
     [SerializeField] float steerSpeed = 1f;
     [SerializeField] float moveSpeed = 0.01f;
+    [SerializeField] float slowSpeed = 5f;
+    [SerializeField] float boostSpeed = 20f;
 
-
-
-    void Start()
-    {
-        
-    }
 
     void Update()
     {
@@ -20,5 +16,18 @@ public class Driver : MonoBehaviour
         float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
         transform.Rotate(0, 0, -steerAmount);
         transform.Translate(0,moveAmount,0);
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        moveSpeed = slowSpeed;    
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Boost")
+        {
+            moveSpeed = boostSpeed;
+        }
     }
 }
